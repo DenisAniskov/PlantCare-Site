6ad1b3a31cc86b501ee59924dc0341da82a32260
DenisAniskov <denis.aniskov55@gmail.com>
2026-04-12 22:49:58 +0300

    chore: cleanup netlify config and update releases

--- git-commit-site.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo === PlantCare-Site ===
-git add Releases/ index.html script.js
-git status
-git commit -m "chore: v0.1.1 - APK, EXE, MSI с fallback бэкендом"
-git push origin main
-pause
